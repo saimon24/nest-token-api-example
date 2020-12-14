b6fd904a3d1a16ca28d56b4ff34309e95328ab3e
Simon Grimm <saimon@devdactic.com>
2020-12-14 12:42:00 +0100

    Initial commit.

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [ UsersModule, AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ ConfigModule ],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      }),
+      inject: [ ConfigService ],
+    })
+  ],
+  controllers: [ ],
+  providers: [ ],
+})
+export class AppModule { }
---
+++ src/users/users.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Post, Body, Put, Param, Delete, Res, HttpStatus, NotFoundException, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async addUser(@Res() res, @Body() createUserDto: CreateUserDto) {
+    try {
+      const user = await this.usersService.create(createUserDto);
+      return res.status(HttpStatus.OK).json({
+        msg: 'User has been created successfully',
+        user
+      });
+    } catch (e) {
+      return res.status(HttpStatus.CONFLICT).json({
+        msg: 'User already exists'
+      });
+    }
+  }
+
+  @UseGuards(AuthGuard())
+  @Get(':userID')
+  async getUser(@Res() res, @Param('userID') userID) {
+    const user = await this.usersService.findOne(userID);
+    if (!user) throw new NotFoundException('User does not exist!');
+    return res.status(HttpStatus.OK).json(user);
+  }
+
+  @UseGuards(AuthGuard())
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  @UseGuards(AuthGuard())
+  @Get('/secret')
+  async getSecretUserInfo(@Res() res) {
+    return res.status(HttpStatus.OK).json({
+      msg: 'This is my secret message from the server!'
+    });
+  }
+}
