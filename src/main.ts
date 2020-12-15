8c97603a4bb24b1fcfb9a955cf7269d754868a70
Simon Grimm <saimon@devdactic.com>
2020-12-15 15:18:36 +0100

    test.

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   const port = process.env.PORT || 5000;
 
   await app.listen(port);
-  console.log('Listening on: ', port)
+  console.log('Listening on: ', port);
+
 }
 bootstrap();
